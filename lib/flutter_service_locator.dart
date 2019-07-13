e913ec52873bf47e8862619ecdcf2d48c2497b2a
Guy Peled <guy.p@taboola.com>
2019-07-13 22:10:08 +0300

    init

--- lib/flutter_service_locator.dart
+++ lib/flutter_service_locator.dart
@@ -1,8 +1,3 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
 library flutter_service_locator;
 
 export 'src/scope.dart';
-
-// TODO: Export any libraries intended for clients of this package.
